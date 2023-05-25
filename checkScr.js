b287b810b495408b34c2a4a20f29a0b006f582c8
Adhyatma Dwivedi <113773621+r00kieAd@users.noreply.github.com>
2023-05-26 04:07:56 +0530

    Update checkScr.js

--- checkScr.js
+++ checkScr.js
@@ -1,6 +1,6 @@
 const scr = screen.width;
 if (scr < 1500) {
-    alert("Your screen is not compatible for this website!");
+    alert("Website not optimized for Mobile or Tablet screens!");
     document.getElementsByClassName("banner")[0].textContent = "Error!";
     document.getElementById("dices").style.visibility = "hidden";
     document.getElementById("players").style.visibility = "hidden";
