e6b7f41cde41786053ccd13bd32b1f34452d610c
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2024-04-27 15:14:04 +0530

    deleted checkScr js file

--- checkScr.js
+++
@@ -1,8 +0,0 @@
-const scr = screen.width;
-if (scr < 1000) {
-    // alert("Website not optimized for Mobile or Tablet screens!");
-    document.getElementsByClassName("banner")[0].textContent = "Website not optimized for Mobile or Tablet screens!";
-    document.getElementById("dices").style.visibility = "hidden";
-    document.getElementById("players").style.visibility = "hidden";
-    document.getElementsByTagName("button")[0].style.visibility = "hidden";
-}
