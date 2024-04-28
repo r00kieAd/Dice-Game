29ba0759363ecf5edede82f4380c4c391bf82d59
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2024-04-28 22:48:16 +0530

    added winning logic and bug fixes

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 // entry point
-
+const threshold = 10;
 $('#roll').click(
     function () {
+        if ($(this).text() == 'Reset') {
+            resetGame();
+            return;
+        }
         startGame();
     }
 )
 
+function resetGame() {
+    $('#pn2').css('color', '#9EDDFF');
+    $('#pn1').css('color', '#9EDDFF');
+    $('#ps2').text('0');
+    $('#ps1').text('0');
+    $('.status').html('Place your bet!');
+    $('.d1').attr('src', 'images/casino.png');
+    $('.d2').attr('src', 'images/casino.png');
+    $('#roll').text('Bet!');
+}
+
 function startGame() {
+    $('#roll').attr('disabled', 'disabled');
     $('.centerDice img').attr('src', 'images/dice.gif');
     shuffleDices(false).then((data) => {
         const s1 = data[0];
         const s2 = data[1];
         $('.centerDice img').attr('src', 'images/dice2.png');
         evaluateScore(s1, s2);
+        $('#roll').removeAttr('disabled');
     });
 }
 
@@ -22,16 +39,16 @@ function shuffleDices(shuffle) {
         const d1 = $('.d1');
         const d2 = $('.d2');
         const interval = setInterval(() => {
-            num1 = Math.floor(Math.random()*6);
-            d1.attr('src', `images/${num1+1}.png`);
-            num2 = Math.floor(Math.random()*6);
-            d2.attr('src', `images/${num2+1}.png`);
+            num1 = Math.floor(Math.random() * 6);
+            d1.attr('src', `images/${num1 + 1}.png`);
+            num2 = Math.floor(Math.random() * 6);
+            d2.attr('src', `images/${num2 + 1}.png`);
             console.log('shuffling');
         }, 100);
         setTimeout(() => {
             if (!shuffle) {
                 clearInterval(interval);
-                resolve([num1+1, num2+1]);
+                resolve([num1 + 1, num2 + 1]);
             }
         }, 3500);
     });
@@ -48,36 +65,43 @@ function evaluateScore(a, b) {
     ps2.text(String(score2));
 
 
-    if (score1 > score2 && score1 < 100) {
-        console.log(`${score1} > ${score2} && ${score1} < 100`);
-        $('.status').text('Player 1 is ahead..');
+    if (score1 == score2) {
+        $('#pn1').css('color', '#4CCD99');
+        $('#pn2').css('color', '#4CCD99');
+        if (score1 < threshold && score2 < threshold) {
+            console.log(`${score1} == ${score2}`);
+            $('.status').text('Scores are tied..');
+        } else {
+            console.log(`${score1} tied with ${score2}`);
+            declareWinner('none');
+        }
+    } else if (score1 > score2) {
         $('#pn1').css('color', '#4CCD99');
         $('#pn2').css('color', '#C70039');
-    } else if (score2 > score1 && score2 < 100) {
-        console.log(`${score2} > ${score1} && ${score2} < 100`);
-        $('.status').text('Player 2 is ahead..');
+        if (score1 >= threshold) {
+            console.log(`${score1} won over ${score2}`);
+            declareWinner('Player1');
+        } else {
+            console.log(`${score1} > ${score2}`);
+            $('.status').text('Player 1 is ahead..');
+        }
+    } else {
         $('#pn2').css('color', '#4CCD99');
         $('#pn1').css('color', '#C70039');
-    } else if (score1 >= 100 && score2 < 100) {
-        console.log(`${score1} >= 100 && ${score2} < 100`);
-        declareWinner('Player1');
-    } else if (score2 >= 100 && score1 < 100) {
-        console.log(`${score2} >= 100 && ${score1} < 100`);
-        declareWinner('Player2');
-    } else if (score1 >= 100 && score1 == score2) {
-        console.log(`${score1} >= 100 ${score1} == ${score2}`);
-        declareWinner('tied')
-    }
-     else {
-        console.log(`${score1} == ${score2}`);
-        $('.status').text('Scores are tied..');
-        $('#pn1').css('color', '#4CCD99');
-        $('#pn2').css('color', '#4CCD99');
+        if (score2 >= threshold) {
+            console.log(`${score2} won over ${score1}`);
+            declareWinner('Player2');
+        } else {
+            console.log(`${score2} > ${score1}`);
+            $('.status').text('Player 2 is ahead..');
+        }
     }
 }
 
 function declareWinner(winner) {
-    if (winner == 'tied') $('.status').html(`It's a tie! <i class="fa-solid fa-ghost"></i>`);
-    if (!winner == 'tied') $('.status').html(`${winner} won the game!! <i class="fa-solid fa-trophy"></i>`);
+    console.log('declaring winner');
+    if (winner == 'none') $('.status').html(`It's a tie! <i class="fa-solid fa-ghost"></i>`);
+    if (winner != 'none') $('.status').html(`${winner} won the game!! <i class="fa-solid fa-trophy"></i>`);
+    $('#roll').text('Reset');
 }
 
